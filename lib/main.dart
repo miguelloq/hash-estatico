f0192663f41a813f3630b0a5229a061d0ee1ae01
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-09-03 21:44:08 -0300

    Continue

--- lib/main.dart
+++ lib/main.dart
@@ -39,55 +39,69 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Hash Index App'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _bucketCountController,
-              decoration: const InputDecoration(labelText: 'Número de Buckets'),
-              keyboardType: TextInputType.number,
+      body: FutureBuilder<List<Tupla>>(
+        future: DataLoader.loadFromFile(),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState==ConnectionState.waiting){
+            return const Center(child: CircularProgressIndicator(),);
+          }
+          if(snapshot.hasError || snapshot.data==null){
+            return const Center(child: Text("Deu ruim"),);
+          }
+          final data = snapshot.data as List<Tupla>;
+          if(data.isEmpty){
+            return const Center(child: Text("Deu ruim"),);
+          }
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _bucketCountController,
+                  decoration: const InputDecoration(labelText: 'Número de Buckets'),
+                  keyboardType: TextInputType.number,
+                ),
+                TextField(
+                  controller: _pageSizeController,
+                  decoration: const InputDecoration(labelText: 'Tamanho da Página'),
+                  keyboardType: TextInputType.number,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    final pageSize = int.tryParse(_pageSizeController.text) ?? 10;
+                    final Tabela dados = Tabela(lista: data);
+                    provider.createBuckets(dados: dados, pageSize: pageSize);
+                  },
+                  child: const Text('Construir Índice'),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _searchKeyController,
+                  decoration: const InputDecoration(labelText: 'Chave de Busca (valor da tupla)'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    final valor = _searchKeyController.text;
+                    provider.search(valor);
+                  },
+                  child: const Text('Buscar'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    final valor = _searchKeyController.text;
+                    provider.tableScan(valor);
+                  },
+                  child: const Text('Table Scan'),
+                ),
+                const SizedBox(height: 20),
+                Text('Resultado da Busca: ${provider.searchResult}'),
+                const SizedBox(height: 20),
+                Text('Resultado do Table Scan: ${provider.tableScanResult}'),
+              ],
             ),
-            TextField(
-              controller: _pageSizeController,
-              decoration: const InputDecoration(labelText: 'Tamanho da Página'),
-              keyboardType: TextInputType.number,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                final pageSize = int.tryParse(_pageSizeController.text) ?? 10;
-                //final Tabela dados = Tabela(lista: List.generate(466000, (index) => Tupla(valor: 'nome $index', chave: index)));
-                //provider.createBuckets(dados: dados, pageSize: pageSize);
-                final dataLoader = await DataLoader.loadFromFile();
-              },
-              child: const Text('Construir Índice'),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _searchKeyController,
-              decoration: const InputDecoration(labelText: 'Chave de Busca (valor da tupla)'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                final valor = _searchKeyController.text;
-                provider.search(valor);
-              },
-              child: const Text('Buscar'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                final valor = _searchKeyController.text;
-                provider.tableScan(valor);
-              },
-              child: const Text('Table Scan'),
-            ),
-            const SizedBox(height: 20),
-            Text('Resultado da Busca: ${provider.searchResult}'),
-            const SizedBox(height: 20),
-            Text('Resultado do Table Scan: ${provider.tableScanResult}'),
-          ],
-        ),
+          );
+        }
       ),
     );
   }
